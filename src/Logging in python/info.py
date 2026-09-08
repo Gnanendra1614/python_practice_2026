1076b6a9df2ae2cac708cbd47fc20199e0cc87e5
gnanendra reddy <avula.g@diggibyte.com>
2026-09-08 17:37:31 +0530

    Committed Logging in python

---
+++ src/Logging in python/info.py
@@ -0,0 +1,9 @@
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+
+logging.debug("Checking user input")
+logging.info("Application started")
+logging.warning("This is a warning")
+logging.error("An error occurred")
+logging.critical("Critical error occurred")
